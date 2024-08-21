fddf0f32656cb8f8128934b4c02607ce148aec64
ViniciusBarrosFiap <rm97824@fiap.com.br>
2024-08-21 09:39:27 -0300

    finalizado

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,9 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
 import nebula from "../img/nebula.jpg";
 import stars from "../img/stars.jpg";
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js";
 
+const monkeyUrl = new URL("../assets/monkey.glb", import.meta.url);
 
 const renderer = new THREE.WebGLRenderer(); //Instanciando o renderizador (canvas)
 
@@ -148,11 +150,71 @@ scene.background = cubeTextureLoader.load([
 const box2Geometry = new THREE.BoxGeometry(4, 4, 4);
 const box2Material = new THREE.MeshBasicMaterial({
     // color: 0x00FF00,
-    map: textureLoader.load(nebula),
+    // map: textureLoader.load(nebula),
 });
-const box2 = new THREE.Mesh(box2Geometry, box2Material);
+
+const box2MultiMaterial = [
+    new THREE.MeshBasicMaterial({map: textureLoader.load(stars)}),
+    new THREE.MeshBasicMaterial({map: textureLoader.load(stars)}),
+    new THREE.MeshBasicMaterial({map: textureLoader.load(nebula)}),
+    new THREE.MeshBasicMaterial({map: textureLoader.load(stars)}),
+    new THREE.MeshBasicMaterial({map: textureLoader.load(nebula)}),
+    new THREE.MeshBasicMaterial({map: textureLoader.load(stars)}),
+
+
+];
+const box2 = new THREE.Mesh(box2Geometry, box2MultiMaterial);
 scene.add(box2);
 box2.position.set(0, 15, 10);
+// box2.material.map = textureLoader.load(nebula);
+
+const plane2Geometry = new THREE.PlaneGeometry(10, 10, 10, 10);
+const plane2Material = new THREE.MeshBasicMaterial({
+    color: 0xFFFFFF,
+    wireframe: true
+});
+const plane2 = new THREE.Mesh(plane2Geometry, plane2Material);
+scene.add(plane2);
+plane2.position.set(10, 10, 15);
+
+plane2.geometry.attributes.position.array[0] -= 10 * Math.random();
+plane2.geometry.attributes.position.array[1] -= 10 * Math.random();
+plane2.geometry.attributes.position.array[2] -= 10 * Math.random();
+const lastPointZ = plane2.geometry.attributes.position.array.length - 1;
+plane2.geometry.attributes.position.array[lastPointZ] -= 10 * Math.random();
+
+const sphere2Geometry = new THREE.SphereGeometry(4);
+// const vShader = `
+//     void main() {
+//         gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+//     }
+// `;
+// const fShader = `
+//     void main() {
+//         gl_FragColor = vec4(0.5, 0.5, 1.0, 1.0);
+//     }
+// `;
+
+
+const sphere2Material = new THREE.ShaderMaterial({
+    vertexShader: document.getElementById('vertexShader').textContent,
+    fragmentShader: document.getElementById('fragmentShader').textContent
+});
+const sphere2 = new THREE.Mesh(sphere2Geometry, sphere2Material);
+scene.add(sphere2);
+sphere2.position.set(-5, 10, 10);
+
+const assetLoader = new GLTFLoader();
+
+assetLoader.load(monkeyUrl.href, function(gltf) {
+    const model = gltf.scene;
+    scene.add(model);
+    model.position.set(-12, 4, 0)
+
+}, undefined, function(error){
+    console.error(error);
+})
+
 
 
 //adicionando o color picker na interface 
@@ -169,9 +231,20 @@ gui.add(options, 'wireframe').onChange(function(e){
 gui.add(options, 'speed', 0, 0.1);
 gui.add(options, "angle", 0, 1);
 gui.add(options, "penumbra", 0, 1);
-gui.add(options, "intensity", 0, 500);
+gui.add(options, "intensity", 1, 500);
 
 let step = 0;
+
+
+const mousePosition = new THREE.Vector2();
+window.addEventListener("mousemove", function(e){
+    mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mousePosition.y = - (e.clientY / window.innerHeight) * 2 + 1; 
+});
+const rayCaster = new THREE.Raycaster();
+
+const sphereId = sphere.id;
+box2.name = "theBox";
 
 //Função responsavel por fazer a animação do cubo adicionado na cena
 function animate(time) {
@@ -186,8 +259,35 @@ function animate(time) {
     spotLight.intensity = options.intensity;
     spotLightHelper.update();
 
+    rayCaster.setFromCamera(mousePosition, camera);
+    const intersects = rayCaster.intersectObjects(scene.children);
+    // console.log(intersects);
+
+    for(let i = 0; i < intersects.length; i++) {
+        if(intersects[i].object.id ===  sphereId) {
+            intersects[i].object.material.color.set(0xFF0000);
+        }
+        if(intersects[i].object.name === 'theBox') {
+            intersects[i].object.rotation.x = time / 1000;
+            intersects[i].object.rotation.y = time / 1000;
+        }
+    };
+
+    plane2.geometry.attributes.position.array[0] = 10 * Math.random();
+    plane2.geometry.attributes.position.array[1] = 10 * Math.random();
+    plane2.geometry.attributes.position.array[2] = 10 * Math.random();
+    plane2.geometry.attributes.position.array[lastPointZ] = 10 * Math.random();
+    plane2.geometry.attributes.position.needsUpdate = true;
+
+
     //A função será chamada em loop, então sempre que a posição estiver alterando ele já irá sendo renderizado na tela
     renderer.render(scene, camera); //renderizando a cena e a camera no canvas
 };
 //Colocando a animação para rodar em loop
 renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', ()=> {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+})
